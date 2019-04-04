0644fed398dd24cf8bd4351a8c0fb407644e7896
Johannes Damp <jodamp@uni-mainz.de>
2019-04-04 18:49:46 +0200

    Implement stochastic gradient descent

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -51,7 +51,9 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      rnd_choice = np.random.choice(num_train, batch_size)
+      X_batch = X[rnd_choice]
+      y_batch = y[rnd_choice]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -65,7 +67,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W -= learning_rate*grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
